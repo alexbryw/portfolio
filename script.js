e5d03d24f04d5ca30c101551db6833c6b54a60bb
alexbryw <alexbryw@gmail.com>
2019-11-18 21:50:03 +0100

    Cancel scroll hint animation

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ window.onload = starter;
 
 function starter(){
     document.addEventListener('scroll', fixNavbar);
+    document.addEventListener('scroll', cancelScrollHint);
+    cancelScrollHint();
 }
 
+/**
+ * Makes the navbar stick to the top of the page after scrolling down past the top image.
+ */
 function fixNavbar(){
-    
     const navbar = document.querySelector('.navbar');
-    console.log('scrolling .. offsetTop '+navbar.offsetTop + ' '+window.pageYOffset);
+    //console.log('scrolling .. offsetTop '+navbar.offsetTop + ' '+window.pageYOffset);
     //navbar.offsetTop
     if(window.pageYOffset >= window.innerHeight){
         navbar.classList.add('fixNavbarToTop');
@@ -16,3 +20,16 @@ function fixNavbar(){
         navbar.classList.remove('fixNavbarToTop');
     }
 }
+
+/**
+ * Cancels scroll hint when user has already scrolled down.
+ */
+function cancelScrollHint(){
+    if(window.pageYOffset > 0){
+        //console.log('scroll from top '+ window.pageYOffset);
+        document.querySelector('.background-holder').style.animationDuration = '0s'
+
+    }
+}
+
+//TODO add css grid 2 colum then one for mobile, autofill?.
