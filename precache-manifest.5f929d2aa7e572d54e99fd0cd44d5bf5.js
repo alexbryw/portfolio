b8322870c0c7e07986e04b0cd643c0ce610a9a1b
alexbryw <alexbryw@gmail.com>
2021-05-03 20:52:14 +0200

    Updates

---
+++ precache-manifest.5f929d2aa7e572d54e99fd0cd44d5bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25db973e72c6d2b1ea57a09612dd906",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e8b63688059bfbf9f108",
+    "url": "/portfolio/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "12b1fd6b29044333efff",
+    "url": "/portfolio/static/js/2.e5cca7a1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.e5cca7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b63688059bfbf9f108",
+    "url": "/portfolio/static/js/main.f105f210.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
